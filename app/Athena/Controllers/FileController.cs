f5ec86141bdf3ce12eaa8adc22f6c219c4e3c9a4
s <hyerim.kwon@it.weltec.ac.nz>
2020-04-15 13:04:21 +1200

    Using JSON instead of YAML to utilize  JsonSerializer.Deserialize() for creating a Deployment.

---
+++ app/Athena/Controllers/FileController.cs
@@ -0,0 +1,114 @@
+﻿/*using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Athena.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            // Specify a file to read from and to create.
+            string pathSource = @"C:\Users\s\source\repos\a.yaml";
+            // string pathNew = @"C:\Users\s\source\repos\a1.yaml";
+
+            try
+            {
+
+                using (FileStream fsSource = new FileStream(pathSource,
+                    FileMode.Open, FileAccess.Read))
+                {
+
+                    // Read the source file into a byte array.
+                    byte[] bytes = new byte[fsSource.Length];
+                    int numBytesToRead = (int)fsSource.Length;
+                    int numBytesRead = 0;
+                    while (numBytesToRead > 0)
+                    {
+                        // Read may return anything from 0 to numBytesToRead.
+                        int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
+
+                        // Break when the end of the file is reached.
+                        if (n == 0)
+                            break;
+
+                        numBytesRead += n;
+                        numBytesToRead -= n;
+                    }
+                    numBytesToRead = bytes.Length;
+
+                    ViewData["Message"] = fsSource;
+
+                    *//* // Write the byte array to the other FileStream.
+                     using (FileStream fsNew = new FileStream(pathNew,
+                             FileMode.Create, FileAccess.Write))
+                         {
+                             fsNew.Write(bytes, 0, numBytesToRead);
+                         }
+                     }*//*
+                }
+            }
+            catch (FileNotFoundException ioEx)
+            {
+                ViewData["Message"] = ioEx.Message;
+            }
+
+
+            return View();
+        }
+    }
+}*/
+
+/*using SharpYaml.Serialization;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    private class Config
+    {
+        public string IndexDir { get; set; }
+        public string LogDir { get; set; }
+        public string[] FilterPattern { get; set; }
+        public string[] SomeOptionalProp { get; set; }
+
+        public override string ToString()
+        {
+            var bldr = new StringBuilder();
+
+            bldr.AppendLine(string.Format("IndexDir - {0}", IndexDir));
+            bldr.AppendLine(string.Format("LogDir - {0}", LogDir));
+            if (FilterPattern != null)
+            {
+                bldr.AppendLine(string.Format("FilterPattern:"));
+                foreach (var fp in FilterPattern)
+                    bldr.AppendLine(string.Format("  - {0}", fp));
+            }
+
+            if (SomeOptionalProp != null)
+            {
+                bldr.AppendLine(string.Format("SomeOptionalProp:"));
+                foreach (var fp in SomeOptionalProp)
+                    bldr.AppendLine(string.Format("  - {0}", fp));
+            }
+
+            return bldr.ToString();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        var yamlPath = "config.yml";
+        var input = new StreamReader(yamlPath);
+
+        var deserializer = new Serializer();
+        var config = (Config)deserializer.Deserialize(input, typeof(Config));
+
+        Console.WriteLine(config.ToString());
+        Console.ReadKey();
+    }
+}*/
---
+++ app/Athena/Controllers/Yaml2Controller.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using k8s.Models;
+using k8s;
+using System.Text;
+
+namespace Athena.Controllers
+{
+    public class Yaml2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            /*var k8SClientConfig = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            var client = new Kubernetes(k8SClientConfig);
+           
+            var namespaces = client.ListNamespace();
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var ns in namespaces.Items)
+            {
+                Console.WriteLine(ns.Metadata.Name);
+                var list = client.ListNamespacedPod(ns.Metadata.Name);
+
+                foreach (var item in list.Items)
+                {
+                    ViewData["Message"] = stringBuilder.Append(item.Metadata.Name);
+                    Console.WriteLine(item.Metadata.Name);
+                }
+            } 
+                return View();*/
+
+             V1Deployment deployment = new V1Deployment()
+             {
+                 ApiVersion = "apps/v1",
+                 Kind = "Deployment",
+                 Metadata = new V1ObjectMeta()
+                 {
+                     Name = "nginx-deployment5",
+                     Labels = new Dictionary<string, string>()
+                             {
+                                 { "app", "nginx" }
+                             }
+                 },
+                 Spec = new V1DeploymentSpec
+                 {
+                     Replicas = 1,
+                     Selector = new V1LabelSelector()
+                     {
+                         MatchLabels = new Dictionary<string, string>
+                                 {
+                                     { "app", "nginx" }
+                                 }
+                     },
+                     Template = new V1PodTemplateSpec()
+                     {
+                         Metadata = new V1ObjectMeta()
+                         {
+                             CreationTimestamp = null,
+                             Labels = new Dictionary<string, string>
+                                     {
+                                          { "app", "nginx" }
+                                     }
+                         },
+                         Spec = new V1PodSpec
+                         {
+                             Containers = new List<V1Container>()
+                                     {
+                                         new V1Container()
+                                         {
+                                             Name = "nginx",
+                                             Image = "nginx:1.14.2",
+                                             Ports = new List<V1ContainerPort> { new V1ContainerPort(80) }
+                                         }
+                                     }
+                         }
+                     }
+                 }
+                 /*Status = new V1DeploymentStatus()
+                 {
+                     Replicas = 1
+                 }*/
+             };
+             
+            var k8SClientConfig = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            var client = new Kubernetes(k8SClientConfig);
+            var result = client.CreateNamespacedDeployment(deployment, "default");
+
+            ViewData["Message"] = result;
+            return View();
+        }
+    }
+}
