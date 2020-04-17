5d473b9c2e6a7a5096c678099aeafe4159fa26a9
s <hyerim.kwon@it.weltec.ac.nz>
2020-04-17 20:29:35 +1200

    EF framework added - database for the core functionality

---
+++ app/Athena/Data/AthenaContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Athena.Models;
+
+namespace Athena.Data
+{
+    public class AthenaContext : DbContext
+    {
+        public AthenaContext(DbContextOptions<AthenaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Label> Label { get; set; }
+        public DbSet<User> User { get; set; }
+    }
+}
+ 
+ 
---
+++ app/Athena/Migrations/20200417064455_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Athena.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Label",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Key = table.Column<string>(nullable: true),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Label", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Label");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
